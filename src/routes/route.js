4de7cd04d3af090f6e79e4f00488871cddb546dc
D2460 <105585011+D2460@users.noreply.github.com>
2022-06-02 12:06:10 +0530

    new push

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,34 +3,37 @@ const express = require('express');
 const externalModule1 = require("../logger/logger")
 const externalModule2 = require("../util/helper")
 const externalModule3 = require("../validator/formatter")
+const underscore = require("underscore")
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     externalModule1.val()
-    externalModule2.Date()
-    externalModule2.Month()
+    externalModule2.Date1()
+    externalModule2.Month2()
     externalModule2.BatchInfo()
     externalModule3.trim1()
     externalModule3.lowerCase()
     externalModule3.upperCase()
+    let firstElement = underscore.first(["Dipen","Ajoy","Anita"])
+    console.log("Arrays first element is: " + firstElement)
     res.send('My first ever api!')
 });
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
+// router.get('/test-me1', function (req, res) {
+//     res.send('My second ever api!')
+// });
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
+// router.get('/test-me2', function (req, res) {
+//     res.send('My third api!')
+// });
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
+// router.get('/test-me3', function (req, res) {
+//     res.send('My 4th api!')
+// });
 
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
+// router.get('/test-me4', function (req, res) {
+//     res.send('My last api!')
+// });
 
 module.exports = router;
 // adding this comment for no reason
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,15 +1,17 @@
-const Date = function printDate() {
-    console.log("Todays Date is: 01.06.2022")
+const Date1 = function () {
+    let currentDate = new Date()
+    console.log(currentDate)
+}
+const Month2 = function () {
+    let currentDate2 = new Date()
+    let currentMonth = currentDate2.getMonth() + 1
+    console.log(currentMonth)
 }
 
-const Month = function printMonth() {
-    console.log("Current Month is June-2022")
-}
-
-const BatchInfo = function getBatchInfo() {
+const BatchInfo = function () {
     console.log("Roadon, W3D3, the topic for today is Nodejs module system")
 }
 
-module.exports.Date = Date
-module.exports.Month = Month
+module.exports.Date1 = Date1
+module.exports.Month2 = Month2
 module.exports.BatchInfo = BatchInfo
