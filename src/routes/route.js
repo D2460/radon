e494f1546ce746da6925c4b5a205b416ce0dc54f
D2460 <105585011+D2460@users.noreply.github.com>
2022-06-05 17:33:55 +0530

    new-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,36 +2,12 @@ const express = require('express');
 const userController = require("../controllers/userControllers")
 const router = express.Router();
 
-router.get('/hello', userController.a);
+router.get('/movies', userController.h);
 
-router.get('/hello2', userController.b)
+router.get('/movies/:indexNumber', userController.k);
 
-router.get('/hello3', userController.c)
+router.get('/films', userController.m);
 
-router.get('/hello4', userController.d)
-
-router.get('/test-me', userController.test1);
-
-router.get('/candidate', userController.Query1);
-
-router.get('/candidate/:name', userController.body1);
-
-router.get('/sol1', userController.Missing2);
-
-router.get('/missingNum', userController.MissingNumSol2);
-
-router.get('/missing', userController.missingNumber);
-
-router.get('/sol2', userController.MissingNum);
-
-router.get('/getUsersData', userController.getUsersData);
-
-router.post('/createUser', userController.createUser);
-
-router.post('/Dipen', userController.test2);
-
-router.post('/players', userController.AddName);
-  
-router.post('/test-post-4', userController.print1);
+router.get('/films/:filmId', userController.w);
 
 module.exports = router;
