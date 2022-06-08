72a76f18124228cfb9988bc7a14bdf65188cbed5
D2460 <105585011+D2460@users.noreply.github.com>
2022-06-08 14:21:06 +0530

    db-2-Assignement

--- src/controllers/bookController_authorController.js
+++ src/controllers/bookController_authorController.js
@@ -15,7 +15,7 @@ const createAuthor= async function (req, res) {
 
 const getBooksChatanBhagat = async function (req, res) {
     let authorId = await AuthorModel.find({author_name:"Chetan Bhagat"}).select({author_id:1, _id:0})
-    let allBooks = await BookModel.find({result:1}).select({name:1, _id:0})
+    let allBooks = await BookModel.find({author_id:authorId[0].author_id}).select({name:1, _id:0})
     console.log(authorId)
     res.send({msg:allBooks})
 }
@@ -24,13 +24,13 @@ const getBooks_update = async function (req, res) {
     let data = req.body
     let bookUpdate = await BookModel.findOneAndUpdate({name:"Two states"},
     {$set:data},{new:true}).select({author_id:1,price:1,name:1,_id:0})
-    let findId = await AuthorModel.find({author_id:2}).select({author_name:1, _id:0})
+    let findId = await AuthorModel.find({author_id:bookUpdate.author_id}).select({author_name:1, _id:0})
     res.send({msg:bookUpdate,findId})
 }
 
 const findAuthor = async function (req, res) {
     let findAuthorId = await BookModel.find({price:{$gt:50,$lt:100}}).select({author_id:1,_id:0})
-    let findAuthorName = await AuthorModel.find({author_id:{$in:[1,2,3]}}).select({author_name:1,_id:0})
+    let findAuthorName = await AuthorModel.find({author_id:{$in:[findAuthorId[0].author_id,findAuthorId[1].author_id,findAuthorId[2].author_id]}}).select({author_name:1,_id:0})
     console.log(findAuthorId)
     const result = findAuthorName.forEach(x => console.log(x))
     res.send({msg:findAuthorName})
