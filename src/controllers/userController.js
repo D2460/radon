d39c1413260117c20edce30fb26009ce63520567
D2460 <105585011+D2460@users.noreply.github.com>
2022-06-13 14:21:38 +0530

    ap

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,28 +10,18 @@ const basicCode= async function(req, res) {
     console.log( "HEADER DATA ABOVE")
     console.log( "hey man, congrats you have reached the Handler")
     res.send({ msg: "This is coming from controller (handler)"})
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 const createUser= async function (req, res) {
     let data= req.body
+    let tokenDataInHeaders= req.headers.token
+    console.log("Request headers before modification: ",req.headers)
+    console.log(req.headers.batch)
+    console.log(req.headers["Content-Type"])
+    console.log(tokenDataInHeaders)
+    req.headers["month"] = "june"
+    console.log("Request headers After modification: ",req.headers)
+    res.headers('year','2022')
     let savedData= await UserModel.create(data)
     res.send({msg: savedData})
 }
