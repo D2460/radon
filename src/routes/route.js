aa13832999270237ccfe4cbd42c2475488162099
D2460 <105585011+D2460@users.noreply.github.com>
2022-06-17 16:14:54 +0530

    c

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,15 +2,10 @@ const express = require('express');
 const router = express.Router();
 const CowinController= require("../controllers/cowinController")
 
-
-
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-
 router.get("/cowin/states", CowinController.getStates)
+
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
+
 router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
