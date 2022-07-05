cdc207f85558674c9cc65467e2805ae7c4fb1e9e
D2460 <dipenruidas1996@gmail.com>
2022-07-05 16:24:40 +0530

    up

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
 export const scaleFactor = 621 / 900
 
 const baseUrl = 'http://localhost:3001/functionup'
-export const collegeDetailsApi = collegeName => `${baseUrl}/collegeDetails?collegeName=${collegeName}`
+export const collegeDetailsApi = collegeName => `${baseUrl}/collegeDetails?name=${collegeName}`
 export const placementInterestApi = `${baseUrl}/interns`
